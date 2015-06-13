effd623b95f9b5755ce4b0798f78090e36676ada
Andrew Monks <a@monks.co>
2015-06-12 22:35:04 -0400

    better documentation

--- engine.js
+++ engine.js
@@ -3,16 +3,23 @@
 var Engine = {};
 
 // function to start the game
+// you'll need to create your own game object and feed it in
+// in this example, that happens in index.html
 Engine.start = function(game) {
+  // add that game to the engine so we can find it later
   Engine.game = game;
+  // start it off!
   Engine.enter(Engine.game.startingPlace);
 };
 
 // function to enter a place
 Engine.enter = function(id) {
-  // enter the new place
   console.log("entering " + id);
+
+  // set the player location to the new place's id
   Engine.game.player.location = id;
+
+  // create a newPlace variable for convenience
   var newPlace = Engine.game.places[id];
 
   // load the new place name
@@ -29,11 +36,12 @@ Engine.enter = function(id) {
 };
 
 // function to set up the destinations list for a place
+// argument: an array of new destination ids: ["concord", "worcester"]
 Engine.setUpDestinations = function(destinations) {
-  // clear existing destinations
+  // clear existing destinations from the list
   $("#destinations").empty();
 
-  // add new destinations to list
+  // add new destinations to the list
   for ( var d in destinations ) {
     var destinationID = destinations[d];
     var destinationPlace = Engine.game.places[destinationID];
@@ -42,6 +50,7 @@ Engine.setUpDestinations = function(destinations) {
 
   // activate links
   $(".destination").click(function() {
+    // `this` is the link itself: <a id='worcester' href='#'>
     Engine.enter(this.id);
   });
 };
