3db0b58653472e9573b6d02c1c908c5885f6b887
Andrew Monks <a@monks.co>
2015-06-15 15:27:08 -0400

    Updates

--- sidevalve.js
+++ sidevalve.js
@@ -314,7 +314,7 @@ var Sidevalve = function() {
 
   // function to show the about box
   showAbout = function() {
-    var aboutText = renderHandlebars('about-text.md');
+    var aboutText = renderHandlebars('about-text.md', API.game);
     bootbox.alert(renderMarkdown(aboutText));
   };
 
@@ -442,23 +442,23 @@ var Sidevalve = function() {
   var preloadImages = function() {
     preloadObjectImages(API.game.places);
     preloadObjectImages(API.game.objects);
-  }
+  };
 
   // function to preload all the images for an object
   // argument: something like API.game.places
   var preloadObjectImages = function(o) {
-    for (i in o) {
+    for (var i in o) {
       var url = o[i].image;
       preloadImage(url);
     }
-  }
+  };
 
   // function to preload an individual image
   var preloadImage = function(url) {
     console.log("preloading " + url);
-    var image = new Image()
+    var image = new Image();
     image.src = url;
-  }
+  };
 
   // the constructor needs to return the public API object
   return API;
